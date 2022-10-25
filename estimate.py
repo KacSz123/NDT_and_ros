b4db2894552bba6c8c5bef61b00d7043b8b17bef
KacSz123 <kacperszcz159@gmail.com>
2022-10-25 12:32:04 +0200

    commit

--- estimate.py
+++ estimate.py
@@ -2,6 +2,14 @@ from ctypes import sizeof
 import numpy as np
 import matplotlib.pyplot as plt
 import random 
+import scipy as sp
+from scipy import stats
+
+fig, ax = plt.subplots()
+def gauss_2d(mu, sigma):
+    x = random.gauss(mu, sigma)
+    y = random.gauss(mu, sigma)
+    return (x, y)
 #random_float_list = []
 size = 10 #m
 range = 0.01
@@ -14,7 +22,7 @@ print(x)
 #wall_x2 = np.arange(0, 11, 0.1) + 33
 print('Values of x: ', x)
 print('Values of y: ', wall_y1)
-plt.figure()
+#plt.figure()
 plt.plot(x, wall_y1,'b')
 plt.plot(x, wall_y2,'b')
 #plt.figure()
@@ -49,10 +57,26 @@ print(sum)
 # plt.scatter(x, wall_y2)
 # plt.scatter(wall_y1, x)
 # plt.scatter(wall_y2, x)
+##proba
+data = []
+datay = []
+for i in np.arange(50):
+    data.append(x[i])
+    datay.append(wall_y1[i])
+out1=stats.norm.pdf(data,mi[0],sum[0])
+xx,yy = np.meshgrid(out1, datay)
+zz=np.sqrt(xx*xx + yy*yy)
+cc=plt.contourf(xx,yy,zz)
+cir1=plt.Circle((mi[0], wall_y2[0]), sum[0]*10, color='r')
+ax.add_patch(cir1)
+#ax.add_patch(cc)
 print(len(wall_y1))
 print(len(mi))
 plt.title("Identity Function")
 plt.xlabel("Values of x")
 plt.ylabel("Values of y")
+
+
+plt.contourf(out1,datay, zz)
 plt.show()
 #print(x)
