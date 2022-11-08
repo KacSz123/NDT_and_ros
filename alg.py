0ff6b92b8584b2cf15dbcdc6212348a0f7fcbf05
KacSz123 <kacperszcz159@gmail.com>
2022-11-08 10:28:24 +0100

    algorithm ver. 1.0

--- alg.py
+++ alg.py
@@ -1,69 +1,115 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
-from ctypes import sizeof
-from heapq import heappush
 from PIL import Image
 import os
+from matplotlib.patches import Ellipse
+import math
+def GetMuSigma(x):
+    mu_x = np.mean(x)
+    sigma_x = np.std(x)
+    return mu_x, sigma_x*sigma_x
+        
+def GetMuSigmaFromEqSqrt(x):
+    suma=0
+    for i in x:
+        suma = suma + i
+    mu_x=sum(x)/len(x)
+    suma=0
+    for i in x:
+        suma = suma + (i-mu_x)*(i-mu_x)
+    sigma_x=suma/len(x)
+    return mu_x, math.sqrt(sigma_x)
+        
+def GetMuSigmaFromEq(x):
+    suma=0
+    for i in x:
+        suma = suma + i
+    mu_x=sum(x)/len(x)
+    suma=0
+    for i in x:
+        suma = suma + (i-mu_x)*(i-mu_x)
+    sigma_x=suma/len(x)
+    return mu_x, sigma_x
+        
+def GetMuSigmaFromAngle(x):
+    suma=0
+    for i in x:
+        suma = suma + i
+    mu_x=sum(x)/len(x)
+    suma=0
+    for i in x:
+        suma = suma + (i-mu_x)*(i-mu_x)
+    sigma_x=suma/len(x)
+    return mu_x, sigma_x
+def ComputeAngle(xsigma,ysigma):
+    return math.degrees(math.asin(xsigma/math.sqrt(xsigma*xsigma+ysigma*ysigma)))
+    
+def main():
+    cell_size = 20
+    img_name = 'img/bigmap.bmp'
 
-cell_size = 50
-img_name = 'img/sth.bmp'
-im = Image.open(img_name, 'r')
-width, height = im.size
-#list2d=[[0 for i in range(width)] for j in range(height)]
-list2d = [[0]*width]*height
-print(len(list2d)*len(list2d[0]))
-print(list2d[10][10])
-pixel_values = list(im.getdata())
+    im = Image.open(img_name, 'r')
+    width, height = im.size
+    pixel_values = list(im.getdata())
 
-cells_horizontal = width//cell_size
-cells_vertical = height//cell_size
-#print(pixel_values)
+    cells_horizontal = width//cell_size
+    cells_vertical = height//cell_size
 
-print(width)
-print(height)
-print(pixel_values[500*83+114])
-print(len(pixel_values))
-for i in range(width*height):
-    list2d[i%500][i//500] = pixel_values[i]
-    #print('[%d][%d]'%(i//500,i%500))
-#print(pixel_values)
+    print('Dim: x=%d, y=%d, pixells:%d' % (width, height, width*height))
+    print('Dim cell:x=%d Cells no. y=%d' % (cell_size, cells_horizontal*cells_vertical))
 
+    x=[]
+    y=[]
+    for i in range(height*width):
+        if (pixel_values[i]==0):
+            x.insert(0,i%width)
+            y.insert(0, height - i//height)
+            
+    current_x = []
+    current_y = []     
+    counter = 0     
+    j=0
+    ells=[]       
+    for i in range(cells_vertical):
+        for j in range(cells_horizontal):
+            counter = 0  
+            for k in range(cell_size):
+                for l in range(cell_size):
+                    if(pixel_values[(i*cell_size+k)*width + j*cell_size+l] == 0):
+                        counter = counter +1
+                        current_x.append((j*cell_size)+l)
+                        current_y.append((height-(i*cell_size+k)))
+            if(counter>5):
+                ymi,ysigma = GetMuSigmaFromEq(current_y)
+                xmi,xsigma = GetMuSigmaFromEq(current_x)
+                #ang=ComputeAngle(xsigma,ysigma)
+                ang=0
+                ells.append( Ellipse(xy=(xmi, ymi), width=xsigma, height=ysigma,angle=ang,
+                                     edgecolor='r', fc='None', lw=0.5))       
+                    
+            current_x.clear()
+            current_y.clear()
 
-x=[]
-y=[]
+    fig, ax = plt.subplots(subplot_kw={'aspect': 'equal'})
+    for e in ells:
+        ax.add_artist(e)
+        e.set_facecolor('grey')     
+    del ells
+        
+    cmd = 'display ' + img_name + '&'
+    os.system(cmd)  
+    plt.xlim([0,width])
+    plt.ylim([0,height])           
 
+    plt.figure(2)
 
-for i in range(height*width):
-    if (pixel_values[i]==0):
-        x.insert(0,i%width)
-        y.insert(0, height - i//height)
-        
-        
-cnt = 0              
-for i in range(cells_vertical):
-    for j in range(cells_horizontal):
-        x=2*2        
-        
-        
-        
-        
-        
-        
-        
-        
-print(x)
-print(len(x))
-plt.xlim([0,width])
-plt.ylim([0,height])
-cmd = 'display ' + img_name + '&'
-os.system(cmd)
-plt.figure(1)
-plt.plot(x,y,'b.', linewidth=1)
-plt.figure(2)
+    plt.xlim([0,width])
+    plt.ylim([0,height])
 
-plt.show()
-#print(list2d)
-        
-        
-        
+    plt.plot(x,y,'b.', linewidth=1)
+    plt.show() 
+    #plt.show()
+    # #print(list2d)
+    
+if __name__ == "__main__":
+    main()
--- main.py
+++
@@ -1,8 +0,0 @@
-list1 = []
-
-list1.append((1,1,1))
-list1.append((1,1,3))
-list1.append((1,1,5))
-list1.append((1,1,4))
-
-print(list1)
